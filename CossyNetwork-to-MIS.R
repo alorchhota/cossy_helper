2dc34997f7f76298e8ffa049581efac4b5847c2a
ashis <alorchhota@gmail.com>
2014-05-23 15:39:27 +0900

    CossyNetwork-to-MIS has been copied from a previous clean implementation.

--- CossyNetwork-to-MIS.R
+++ CossyNetwork-to-MIS.R
@@ -1,7 +1,7 @@
 #####################################################
 ## this script creates MISs from Cossy Network. #####
 ## Cossy networks has 3 types of files. 
-## One file contains the names of pathways (subnetworks), FileName: pathway-***.txt
+## One file contains the names of pathways (subnetworks), FilePAth: data/[NETWORK]/[NETWORK].txt
 ## Node files (FileName: node-[PATHWAY].txt) contains a map from node id to genes.
 ## Edge files (FileName: edge-[PATHWAY].txt) contains the edges
 ## all generated MISs are saved in a gmt file.
@@ -10,53 +10,47 @@
 library(igraph)
 
 ## settings
-minNodeInGis <- 5
-maxNodeInGis <- 25
-directed <- F
-approximate <- T
-rmvMultipleEdges <- T      # it is required for generating mis from kegg network, not for string.
-
-invalid_gene_id <- "2147483647"
-
-## input-output files
-pathwaysDataDir <- "data/networks/kegg"
-pathwaysFileName <- "pathways-kegg.txt"
-gmtOutFile <- paste("results/", pathwaysFileName, "-mis-", minNodeInGis, "-", maxNodeInGis, ".gmt", sep="")
-
-
-
-
-bigGisSizes <- c()
-
-bigGisLines <- c()
-smallGisLines <- c()
-okGisLines <- c()
-
-extractGisFromKgml <- function (kgmlNodeFile, kgmlEdgeFile, rmvMultipleEdges=F){
-  edgeData <- read.table(kgmlEdgeFile, col.names=c("from","to"))
-  edgeData <- edgeData+1
+mis_init <- function(){
+  allowedNetworks <<- c("kegg", "string", "pathwayapi")
+  network <<- "kegg"
+  minNodeInGis <<- 5
+  maxNodeInGis <<- 15
   
-  ## TODO: what to return
+  directed <<- FALSE     # molecular interaction networks are undirected
+  invalid_gene_id <<- "2147483647"
+  
+  
+  gmtOut <<- NA
+  gmtCounter <<- 0
+  gmtPathway <<- "dummy"
+  
+  ##count
+  lowSizeMis <<- 0
+  correctSizeMis <<- 0
+  bigSizeMis <<- 0
+  allLowSizeMis <<- data.frame(id=c(), pathway=c(), genes=c(), stringsAsFactors=F)
+  allCorrectSizeMis <<- data.frame(id=c(), pathway=c(), genes=c(), stringsAsFactors=F)
+  allBigSizeMis <<- data.frame(id=c(), pathway=c(), genes=c(), stringsAsFactors=F)
+  allGis <<- data.frame(id=c(), pathway=c(), genes=c(), stringsAsFactors=F)  
+}
+
+extractGisFromKgml <- function (kgmlNodeFile, kgmlEdgeFile){
+  nodeData <- read.table(kgmlNodeFile, sep="\t", col.names=c("id","genes"), colClasses=c("numeric","character"))
+  edgeData <- read.table(kgmlEdgeFile, col.names=c("from","to"))
+  
   if(nrow(edgeData) <=0 )
     return (-1) 
   
-  ## remove multiple edges for undirected graph
-  if(rmvMultipleEdges)
+  ## kegg network may have multiple edges between same pair of nodes.
+  if(network=="kegg")
     edgeData <- removeMultipleEdges(edgeData, directed)
   
-  
-  
-  nodeData <- read.table(kgmlNodeFile, sep="\t", col.names=c("id","genes"), colClasses=c("numeric","character"))
-  nodeData$id <- nodeData$id+1
   
   kgmlGraph <- buildIGraph(nodeData, edgeData, directed)
   components <- clusters(graph=kgmlGraph)
   
   # for each component, extract GIS
   for(comp in 1:components$no){
-    
-#     if(comp==3)
-#       debug(extractGisFromConnectedGraph)
     
     componentNodes <- which(components$membership==comp)
     componentGraph <- induced.subgraph(graph=kgmlGraph, vids=componentNodes)
@@ -65,16 +59,17 @@ extractGisFromKgml <- function (kgmlNodeFile, kgmlEdgeFile, rmvMultipleEdges=F){
     componentNodeData$id <- 1:nrow(componentNodeData)
     componentEdgeData <- get.edgelist(graph=componentGraph, names=F)
     
-    gisLines <- extractGisFromConnectedGraph(componentNodeData, componentEdgeData)
-    saveGisLines(gisLines)
-    
     ##count low size network
     if(nrow(componentNodeData) < minNodeInGis){
-      smallGisLines <<- c(smallGisLines, gisLines)
+      lowSizeMis <<- lowSizeMis + 1
     }
     else if(nrow(componentNodeData) <= maxNodeInGis){
-      okGisLines <<- c(okGisLines, gisLines)
+      correctSizeMis <<- correctSizeMis + 1
     }
+    
+    
+    gisLines <- extractGisFromConnectedGraph(componentNodeData, componentEdgeData)
+    saveGisLines(gisLines)
   }
   
 }
@@ -128,18 +123,15 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
   kgmlGraph <- buildIGraph(nodeData, edgeData, directed)
   noOfNodes <- vcount(kgmlGraph)
   
-  if(approximate)
-    bet <- fastgreedy.community(kgmlGraph)
-  else
-    bet <- edge.betweenness.community(kgmlGraph)
   
+  ########## build the community dendrogram (graphList) ##########
+  bet <- fastgreedy.community(kgmlGraph)
   
   graphList <- apply(nodeData, 1, function(node){
     g <- graph.empty(n=0, directed=F)
     g <- set.graph.attribute(graph=g, name="genes", value=node["genes"])
     g <- set.graph.attribute(graph=g, name="count", value=1)        # 1 node, count=1
     g <- set.graph.attribute(graph=g, name="nodes", value=as.integer(node["id"]))
-    #g <- set.graph.attribute(graph=g, name="edge", value="")
     return(g)
   })
   
@@ -155,7 +147,7 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
   }
   apply(bet$merges, 1, mergeWithGraphList)
   
-  # find community
+  ######### find appropriate community ##########
   isTaken <- rep(FALSE, graphListLength)
   takenGraphs <- list()
   discardedGraphs <- list()
@@ -196,10 +188,11 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
     return(NA)
   }
   
+  # last row in 'merges' is the first split, so loop from last to first row.
   lapply(nrow(bet$merges):1, findCommunityGraphs)
   
   
-  # take discarded single nodes
+  # update discardedNodeList by adding discarded single nodes [graphIndex: 1:noOfNodes]
   takeDiscardedSingleNodes <- function(nodeIndex){
     if(!isTaken[nodeIndex]){
       discardedGraphs[[discardedGraphsCount+1]] <<- graphList[[nodeIndex]]
@@ -210,19 +203,8 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
   lapply(1:noOfNodes, takeDiscardedSingleNodes)
   
   
-#   if(length(takenGraphs)==0 && length(discardedGraphs)==1){
-#     # The graph has less than minNodeInGis
-#     # prepare a gis and return it
-#     gisGenes <- genesInAnnotatedGraph(discardedGraphs[[1]])
-#     return(gisGenes)
-#   }
-#   
-#   if(length(takenGraphs)==1 && length(discardedGraphs)==0){
-#     # all genes are taken in one graph i.e. give graph size is within the range
-#     gisGenes <- genesInAnnotatedGraph(takenGraphs[[1]])
-#     return(gisGenes)
-#   }
   
+  ############## merge discarded nodes with the closest community ############
   
   # find the nodes within 'among' that have distance d from n
   assignNode <- function(n, among, d){
@@ -295,36 +277,15 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
     return(freq)
   }
   
-#   addNodesInGisGraphs <- function(discardedNodes, memberships){
-#     discardedNodeIndex <- 0
-#     addedNodes <- c()
-#     lapply(discardedNodes, function(dn){
-#       discardedNodeIndex <<- discardedNodeIndex + 1
-#       membershipGraphIndex <- memberships[discardedNodeIndex]
-#       nodeIndex <- discardedNodes[discardedNodeIndex]
-#       if(membershipGraphIndex > 0){
-#         takenGraphs[[membershipGraphIndex]] <<- mergeGraphs(takenGraphs[[membershipGraphIndex]], graphList[[nodeIndex]])
-#         addedNodes[length(addedNodes)+1] <<- nodeIndex
-#       }
-#     })
-#     
-#     return(addedNodes)
-#   }
   
-  # if any node or community is discarded, assign them to some taken graph.
+  # if nodes are discarded, assign them to some taken graph.
   if(length(discardedGraphs) > 0){
     takenNodes <- getAllNodes(takenGraphs)
     discardedNodes <- getAllNodes(discardedGraphs)
     
     while(length(discardedNodes) > 0){
-      print(paste(format(Sys.time(), "%X"), "#discarded nodes:",length(discardedNodes)))
+      #print(paste(format(Sys.time(), "%X"), "#discarded nodes:",length(discardedNodes)))
       memberships <- sapply(discardedNodes, assignNode, takenNodes, 1)
-      
-#       testDebug <- function(){
-#         tmp <- 0
-#       }
-#       debug(testDebug)
-#       testDebug()
       
       if(sum(memberships>0) == 0){
         # no new memberships, so break merging
@@ -356,29 +317,23 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
       takenNodes <- c(takenNodes, newlyAddedNodes)
       
     }
-    print(paste(format(Sys.time(), "%X"), "#discarded nodes:",length(discardedNodes)))
+    #print(paste(format(Sys.time(), "%X"), "#discarded nodes:",length(discardedNodes)))
     
   }
   
+  
+  ################ create MIS if size is within range, otherwise recursively generate MIS ##############
+  
   if(takenGraphsCount==1){
     # could not divide more, so return it. Or divide in special way
-    #print("Could not resplit")
-    bigGisSizes[length(bigGisSizes)+1] <<- get.graph.attribute(graph=takenGraphs[[1]], name="count")
-    gisGenes <- genesInAnnotatedGraph(takenGraphs[[1]])
     
     ##count
-    bigGisLines <<- c(bigGisLines, gisGenes)
+    bigSizeMis <<- bigSizeMis + 1
     
+    gisGenes <- genesInAnnotatedGraph(takenGraphs[[1]])
     return(gisGenes)
   }
   else{
-    # gisGenes
-    # for every taken graph
-    #   if(graph size <= maxThreshold)
-    #     gisGenes.add(gisFromGraphs)
-    #   else  
-    #     create new node and edge data and recursively call this method
-    #     gisGenes.add(result from above method)
     
     gisGenes <- c()
     lapply(takenGraphs, function(tg){
@@ -388,11 +343,11 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
         gisGenes <<- c(gisGenes, gline)
         
         ## count
-        okGisLines[length(okGisLines)+1] <<- gline
+        correctSizeMis <<- correctSizeMis + 1
       }
       else{
         
-        print(paste(format(Sys.time(), "%X"), "re-split:", tg.count))
+        #cat(paste("split network - #nodes:", tg.count, "\n"))
         
         nodeIndexes <- getIntegerNodeVector(tg)
         nodeIndexes <- sort(x=nodeIndexes, decreasing=F)
@@ -403,7 +358,6 @@ extractGisFromConnectedGraph <- function(nodeData, edgeData){
         tgEdgeData <- get.edgelist(graph=tgGraph, names=F)
         
         tgGisGenes <- extractGisFromConnectedGraph(tgNodeData, tgEdgeData)
-        #print(tgGisGenes)
         gisGenes <<- c(gisGenes, tgGisGenes)
       }
     })
@@ -460,12 +414,6 @@ genesInGraph <- function(nodeData){
   return(getValidSortedUniqueGenes(genes))
 }
 
-trim <- function(str){
-  str <- gsub(pattern="^[ \t]*", replacement="", x=str)
-  str <- gsub(pattern="*[ \t]*$", replacement="", x=str)
-  str
-}
-
 getValidSortedUniqueGenes <- function(g.genes){
   g.genes <- strsplit(x=g.genes, split=",")[[1]]
   g.genes <- sort(unique(g.genes))
@@ -477,7 +425,6 @@ getValidSortedUniqueGenes <- function(g.genes){
   
   line <- ""
   lapply(g.genes, function(gid){
-    gid <- trim(gid)
     if(line=="") 
       line<<-gid
     else
@@ -525,7 +472,6 @@ getAllNodes <- function(graphList){
 }
 
 
-allGis <- data.frame(id=c(), pathway=c(), genes=c(), stringsAsFactors=F)
 saveGisLines <- function(gisGeneLines){
   
   lapply(gisGeneLines, function(line){
@@ -564,52 +510,59 @@ printInGmtFile <- function(){
 
 #debug(extractGisFromConnectedGraph)
 
-### Extraction process ############
-
-#read dataset
-pathwaysFile <- paste0(pathwaysDataDir, "/", pathwaysFileName)
-pathways <- read.table(pathwaysFile)
-
-#### debug test for one pathway
-#pathways <- pathways[229,,drop=F]
-#pathways <- pathways[219,,drop=F]
-
-
-## create gmt output file
-gmtOut <- file(gmtOutFile, open="w+")
-gmtCounter <- 0
-gmtPathway <- "dummy"
-
-#debug(saveGis)
-apply(pathways, 1, function(pathwayName){
-  kgmlNodeFile <- paste(pathwaysDataDir, "/node-", pathwayName, ".txt", sep="")
-  kgmlEdgeFile <- paste(pathwaysDataDir, "/edge-", pathwayName, ".txt", sep="")
-  gmtPathway <<- pathwayName
+generate_mis <- function(network, range=NA, outputFile=paste0("results/", network, ".gmt")){
   
-  #print(c(gmtPathway, nrow(allGis)) )
-  print(gmtPathway)
-  extractGisFromKgml(kgmlNodeFile, kgmlEdgeFile, rmvMultipleEdges=rmvMultipleEdges)
+  mis_init()
   
-})
-
-#saveGisLines(okGisLines)
-nokgis <- nrow(allGis)
-print(paste("ok gis:", nokgis))
-
-#saveGisLines(bigGisLines)
-nbiggis <- nrow(allGis) - nokgis
-print(paste("big gis:", nbiggis))
-
-#saveGisLines(smallGisLines)
-nsmallgis <- nrow(allGis) - nokgis - nbiggis
-print(paste("small gis:", nsmallgis))
-
-
-
-printInGmtFile()
-cat(gmtOutFile, "\n")
-close(gmtOut)
-
-cat("#GIS(Total):", nrow(allGis), "\n")
-cat("#GIS(Big):", length(bigGisSizes), "\n")
-cat("Big Sizes:", bigGisSizes, sep="\t")
+  if(!(network %in% allowedNetworks)){
+    stop("Please give one of the allowed networks (kegg/string).")
+  }
+  
+  if(length(range)==1 && is.na(range)){
+    range <- switch(network, kegg=c(5,15), string=c(5,25), pathwayapi=c(5,15))
+  }
+  
+  if(class(range) !=  "numeric" || length(range)!=2 || any(range<=0) ){
+    stop("Range must be a positive numeric array of length 2.")
+  }
+  
+  if(network=="kegg"){
+    cat("Please wait. It may take several minutes. Intel core-i5, 3.3GHz processor with 12GB ram takes about a minute.\n")
+  }
+  else if(network=="string"){
+    cat("Please wait, it may take several minutes. Intel core-i5, 3.3GHz processor with 12GB ram takes about 15 minutes.\n")
+  }
+  else{
+    cat("Please wait, it may take several minutes.\n")
+  }
+  
+  #### setttings ##########
+  network <<- network
+  minNodeInGis <<- min(range)
+  maxNodeInGis <<- max(range)
+  
+  #pathwaysFile <- switch(network, kegg="data/networks/kegg/kegg.txt", string="data/networks/string/string.txt")
+  pathwaysFile <- paste0("data/networks/", network,"/", network,".txt")
+  #gmtOutFile <- paste0("results/", network, ".gmt")
+  gmtOutFile <- outputFile
+  
+  gmtOut <<- file(gmtOutFile, open="w+")
+  gmtCounter <<- 0
+  gmtPathway <<- "dummy"
+  
+  #### generate MIS from each pathway file ########
+  pathways <- read.table(pathwaysFile)
+  apply(pathways, 1, function(pathwayName){
+    kgmlNodeFile <- paste("data/networks/", network, "/node-", pathwayName, ".txt", sep="")
+    kgmlEdgeFile <- paste("data/networks/", network, "/edge-", pathwayName, ".txt", sep="")
+    gmtPathway <<- pathwayName
+    
+    #cat(paste("processing file - ",gmtPathway,"\n"))
+    extractGisFromKgml(kgmlNodeFile, kgmlEdgeFile)
+    
+  })
+  
+  printInGmtFile()
+  cat(paste("MISs are save in the file: ", getwd(),"/", gmtOutFile, "\n", sep=""))  
+  close(gmtOut)
+}
